pardnchiu/golang-ip-guardian

feat: add GeoLite2 location-based risk detection

type.go
@@ -13,6 +13,7 @@ const (
 	redisSessionIP    = "session:ip:%s"
 	redisIPDevice     = "ip:device:%s"
 	redisDeviceFp     = "device:fp:%s"
+	redisGeoIP        = "geo:ip:%s"
 	redisGeoLocation  = "geo:locations:%s"
 	redisIntervalLast = "interval:last:%s"
 	redisSessionStart = "session:start:%s"
@@ -24,6 +25,8 @@ const (
 	redisBlock        = "block:%s"
 	redisBlockCount   = "block:count:%s"
 	redisFrequency    = "frequency:%s:%d"
+	redisLoginFailure = "login:failure:%s"
+	redisNotFound404  = "notfound:404:%s"
 )
 
 var (
@@ -45,9 +48,17 @@ type Config struct {
 	Redis     Redis        `json:"redis"`
 	Email     *EmailConfig `json:"email"`
 	Log       LogConfig    `json:"log"`
+	Filepath  Filepath     `json:"filepath"`
 	Parameter Parameter    `json:"parameter"`
 	// AbuseIPDBToken  string       `json:"abuseipdb_token"`
 	// AbuseIPDBIsPaid bool         `json:"abuseipdb_is_paid"`
+}
+
+type Filepath struct {
+	CityDB    string `json:"city_db"`
+	CountryDB string `json:"country_db"`
+	TrustList string `json:"trust_list"`
+	BanList   string `json:"ban_list"`
 }
 
 type EmailConfig struct {
@@ -60,40 +71,44 @@ type EmailConfig struct {
 }
 
 type Parameter struct {
-	BlockToBan             int `json:"block_to_ban"`              // 封鎖到禁止的次數
-	BlockTimeMin           int `json:"block_time_min"`            // 最小封鎖時間（秒）
-	BlockTimeMax           int `json:"block_time_max"`            // 最大限制時間（秒）
-	RateLimitNormal        int `json:"rate_limit_normal"`         // 正常請求速率限制
-	RateLimitSuspicious    int `json:"rate_limit_suspicious"`     // 可疑請求速率限制
-	RateLimitDangerous     int `json:"rate_limit_dangerous"`      // 危險請求速率限制
-	SessionMultiIP         int `json:"session_multi_ip"`          // 單一 Session 允許的最大 IP 數
-	IPMultiDevice          int `json:"ip_multi_device"`           // 單一 IP 允許的最大設備數
-	DeviceMultiIP          int `json:"device_multi_ip"`           // 單一設備允許的最大 IP 數
-	LoginFailure           int `json:"login_failure"`             // 單一 Session 允許的最大登入失敗次數
-	NotFound404            int `json:"not_found_404"`             // 單一 Session 允許的最大 404 請求數
-	ScoreNormal            int `json:"score_normal"`              // 正常請求的風險分數
-	ScoreSuspicious        int `json:"score_suspicious"`          // 可疑請求的風險分數
-	ScoreDangerous         int `json:"score_dangerous"`           // 危險請求的風險分數
-	ScoreSessionMultiIP    int `json:"score_session_multi_ip"`    // 單一 Session 允許的最大 IP 數可疑分數
-	ScoreIPMultiDevice     int `json:"score_ip_multi_device"`     // 單一 IP 允許的最大設備數可疑分數
-	ScoreDeviceMultiIP     int `json:"score_device_multi_ip"`     // 單一設備允許的最大 IP 數可疑分數
-	ScoreFpMultiSession    int `json:"score_fp_multi_session"`    // 單一指紋允許的最大 Session 數可疑分數
-	ScoreGeoHopping        int `json:"score_geo_hopping"`         // 地理位置跳躍可疑分數
-	ScoreGeoFrequentSwitch int `json:"score_geo_frequent_switch"` // 地理位置頻繁切換可疑分數
-	ScoreGeoRapidChange    int `json:"score_geo_rapid_change"`    // 地理位置快速變化可疑分數
-	ScoreIntervalRequest   int `json:"score_interval_request"`    // 短時間內的請求數可疑分數
-	ScoreFrequencyRequest  int `json:"score_frequency_request"`   // 請求頻率可疑分數
-	ScoreLongConnection    int `json:"score_long_connection"`     // 長連接可疑分數
+	HighRiskCountry        []string `json:"high_risk_country"`         // 高風險國家列表
+	BlockToBan             int      `json:"block_to_ban"`              // 封鎖到禁止的次數
+	BlockTimeMin           int      `json:"block_time_min"`            // 最小封鎖時間（秒）
+	BlockTimeMax           int      `json:"block_time_max"`            // 最大限制時間（秒）
+	RateLimitNormal        int      `json:"rate_limit_normal"`         // 正常請求速率限制
+	RateLimitSuspicious    int      `json:"rate_limit_suspicious"`     // 可疑請求速率限制
+	RateLimitDangerous     int      `json:"rate_limit_dangerous"`      // 危險請求速率限制
+	SessionMultiIP         int      `json:"session_multi_ip"`          // 單一 Session 允許的最大 IP 數
+	IPMultiDevice          int      `json:"ip_multi_device"`           // 單一 IP 允許的最大設備數
+	DeviceMultiIP          int      `json:"device_multi_ip"`           // 單一設備允許的最大 IP 數
+	LoginFailure           int      `json:"login_failure"`             // 單一 Session 允許的最大登入失敗次數
+	NotFound404            int      `json:"not_found_404"`             // 單一 Session 允許的最大 404 請求數
+	ScoreNormal            int      `json:"score_normal"`              // 正常請求的風險分數
+	ScoreSuspicious        int      `json:"score_suspicious"`          // 可疑請求的風險分數
+	ScoreDangerous         int      `json:"score_dangerous"`           // 危險請求的風險分數
+	ScoreSessionMultiIP    int      `json:"score_session_multi_ip"`    // 單一 Session 允許的最大 IP 數可疑分數
+	ScoreIPMultiDevice     int      `json:"score_ip_multi_device"`     // 單一 IP 允許的最大設備數可疑分數
+	ScoreDeviceMultiIP     int      `json:"score_device_multi_ip"`     // 單一設備允許的最大 IP 數可疑分數
+	ScoreFpMultiSession    int      `json:"score_fp_multi_session"`    // 單一指紋允許的最大 Session 數可疑分數
+	ScoreGeoHighRisk       int      `json:"score_geo_high_risk"`       // 高風險地理位置可疑分數
+	ScoreGeoHopping        int      `json:"score_geo_hopping"`         // 地理位置跳躍可疑分數
+	ScoreGeoFrequentSwitch int      `json:"score_geo_frequent_switch"` // 地理位置頻繁切換可疑分數
+	ScoreGeoRapidChange    int      `json:"score_geo_rapid_change"`    // 地理位置快速變化可疑分數
+	ScoreIntervalRequest   int      `json:"score_interval_request"`    // 短時間內的請求數可疑分數
+	ScoreFrequencyRequest  int      `json:"score_frequency_request"`   // 請求頻率可疑分數
+	ScoreLongConnection    int      `json:"score_long_connection"`     // 長連接可疑分數
+	ScoreLoginFailure      int      `json:"score_login_failure"`       // 登入失敗可疑分數
+	ScoreNotFound404       int      `json:"score_not_found_404"`       // 404 請求可疑分數
 }
 
 type IPGuardian struct {
-	Context context.Context
-	Config  *Config
-	Redis   *redis.Client
-	Logger  *Logger
-	// GeoChecker   *GeoCheck
+	Context  context.Context
+	Config   *Config
+	Redis    *redis.Client
+	Logger   *Logger
+	GeoLite2 *GeoLite2
+	Manager  *Manager
 	// AbuseIPDBApi *AbuseIPDBApi
-	Manager *Manager
 }
 
 type Manager struct {
@@ -101,19 +116,6 @@ type Manager struct {
 	Ban   *BanManager
 	Block *BlockManager
 }
-
-// type GeoCheck struct {
-// 	DB                *geoip2.Reader
-// 	highRiskCountries map[string]bool
-// 	proxyRanges       []string // 代理 IP 範圍
-// 	vpnRanges         []string // VPN IP 範圍
-// }
-
-// type GeoRisk struct {
-// 	HighRiskCountries []string `json:"high_risk_countries"`
-// 	ProxyDetection    bool     `json:"proxy_detection"`
-// 	VPNDetection      bool     `json:"vpn_detection"`
-// }
 
 type Redis struct {
 	Host     string `json:"host"`

pardnchiu/go-ip-sentry

type.go
@@ -1,29 +1,48 @@
-package golangIPGuardian
+package golangIPSentry
 
 import (
 	"context"
 	"sync"
+	"time"
 
+	goLogger "github.com/pardnchiu/go-logger"
 	"github.com/redis/go-redis/v9"
 )
 
+type Log = goLogger.Log
+type Logger = goLogger.Logger
+
 const (
-	sessionKey        = "conn.sess.id"
-	deviceKey         = "conn.device.id"
+	sessionKey = "conn.sess.id"
+	deviceKey  = "conn.device.id"
+)
+
+const (
 	redisSessionIP    = "session:ip:%s"
 	redisIPDevice     = "ip:device:%s"
 	redisDeviceFp     = "device:fp:%s"
+	redisGeoIP        = "geo:ip:%s"
 	redisGeoLocation  = "geo:locations:%s"
 	redisIntervalLast = "interval:last:%s"
 	redisSessionStart = "session:start:%s"
 	redisFpSession    = "fp:session:%d:%s"
 	redisInterval     = "interval:%s"
 	redisSuspicious   = "suspicious:%d:%s"
-	redisTrust        = "trust:%s"
-	redisBan          = "ban:%s"
+	redisAllow        = "allow:%s"
+	redisDeny         = "deny:%s"
 	redisBlock        = "block:%s"
 	redisBlockCount   = "block:count:%s"
 	redisFrequency    = "frequency:%s:%d"
+	redisLoginFailure = "login:failure:%s"
+	redisNotFound404  = "notfound:404:%s"
+)
+
+const (
+	defaultLogPath       = "./logs/mysqlPool"
+	defaultLogMaxSize    = 16 * 1024 * 1024
+	defaultLogMaxBackup  = 5
+	defaultWhiteListPath = "./whiteList.json"
+	defaultBlackListPath = "./blackList.json"
 )
 
 var (
@@ -44,82 +63,92 @@ var internalIPs = []string{
 type Config struct {
 	Redis     Redis        `json:"redis"`
 	Email     *EmailConfig `json:"email"`
-	Log       LogConfig    `json:"log"`
+	Log       *Log         `json:"log"`
+	Filepath  Filepath     `json:"filepath"`
 	Parameter Parameter    `json:"parameter"`
 	// AbuseIPDBToken  string       `json:"abuseipdb_token"`
 	// AbuseIPDBIsPaid bool         `json:"abuseipdb_is_paid"`
 }
 
+type Filepath struct {
+	CityDB    string `json:"city_db"`
+	CountryDB string `json:"country_db"`
+	WhiteList string `json:"trust_list"`
+	BlackList string `json:"ban_list"`
+}
+
 type EmailConfig struct {
-	Host     string   `json:"host"`
-	Port     int      `json:"port"`
-	Username string   `json:"username"`
-	Password string   `json:"password"`
-	From     string   `json:"from"`
-	To       []string `json:"to"`
+	Host     string                                 `json:"host"`
+	Port     int                                    `json:"port"`
+	Username string                                 `json:"username"`
+	Password string                                 `json:"password"`
+	From     string                                 `json:"from"`
+	To       []string                               `json:"to"`
+	CC       []string                               `json:"cc"`
+	Subject  *func(ip string, reason string) string `json:"-"` // default: "[IP Sentry] IP {ip} has been banned"
+	Body     *func(ip string, reason string) string `json:"-"` // default: "[IP Sentry] IP {ip} has been banned for {reason}"
 }
 
 type Parameter struct {
-	BlockToBan             int `json:"block_to_ban"`              // 封鎖到禁止的次數
-	BlockTimeMin           int `json:"block_time_min"`            // 最小封鎖時間（秒）
-	BlockTimeMax           int `json:"block_time_max"`            // 最大限制時間（秒）
-	RateLimitNormal        int `json:"rate_limit_normal"`         // 正常請求速率限制
-	RateLimitSuspicious    int `json:"rate_limit_suspicious"`     // 可疑請求速率限制
-	RateLimitDangerous     int `json:"rate_limit_dangerous"`      // 危險請求速率限制
-	SessionMultiIP         int `json:"session_multi_ip"`          // 單一 Session 允許的最大 IP 數
-	IPMultiDevice          int `json:"ip_multi_device"`           // 單一 IP 允許的最大設備數
-	DeviceMultiIP          int `json:"device_multi_ip"`           // 單一設備允許的最大 IP 數
-	LoginFailure           int `json:"login_failure"`             // 單一 Session 允許的最大登入失敗次數
-	NotFound404            int `json:"not_found_404"`             // 單一 Session 允許的最大 404 請求數
-	ScoreNormal            int `json:"score_normal"`              // 正常請求的風險分數
-	ScoreSuspicious        int `json:"score_suspicious"`          // 可疑請求的風險分數
-	ScoreDangerous         int `json:"score_dangerous"`           // 危險請求的風險分數
-	ScoreSessionMultiIP    int `json:"score_session_multi_ip"`    // 單一 Session 允許的最大 IP 數可疑分數
-	ScoreIPMultiDevice     int `json:"score_ip_multi_device"`     // 單一 IP 允許的最大設備數可疑分數
-	ScoreDeviceMultiIP     int `json:"score_device_multi_ip"`     // 單一設備允許的最大 IP 數可疑分數
-	ScoreFpMultiSession    int `json:"score_fp_multi_session"`    // 單一指紋允許的最大 Session 數可疑分數
-	ScoreGeoHopping        int `json:"score_geo_hopping"`         // 地理位置跳躍可疑分數
-	ScoreGeoFrequentSwitch int `json:"score_geo_frequent_switch"` // 地理位置頻繁切換可疑分數
-	ScoreGeoRapidChange    int `json:"score_geo_rapid_change"`    // 地理位置快速變化可疑分數
-	ScoreIntervalRequest   int `json:"score_interval_request"`    // 短時間內的請求數可疑分數
-	ScoreFrequencyRequest  int `json:"score_frequency_request"`   // 請求頻率可疑分數
-	ScoreLongConnection    int `json:"score_long_connection"`     // 長連接可疑分數
+	HighRiskCountry        []string      `json:"high_risk_country"`         // 高風險國家列表
+	BlockToBan             int           `json:"block_to_ban"`              // 封鎖到禁止的次數
+	BlockTimeMin           time.Duration `json:"block_time_min"`            // 最小封鎖時間
+	BlockTimeMax           time.Duration `json:"block_time_max"`            // 最大限制時間
+	RateLimitNormal        int           `json:"rate_limit_normal"`         // 正常請求速率限制
+	RateLimitSuspicious    int           `json:"rate_limit_suspicious"`     // 可疑請求速率限制
+	RateLimitDangerous     int           `json:"rate_limit_dangerous"`      // 危險請求速率限制
+	SessionMultiIP         int           `json:"session_multi_ip"`          // 單一 Session 允許的最大 IP 數
+	IPMultiDevice          int           `json:"ip_multi_device"`           // 單一 IP 允許的最大設備數
+	DeviceMultiIP          int           `json:"device_multi_ip"`           // 單一設備允許的最大 IP 數
+	LoginFailure           int           `json:"login_failure"`             // 單一 Session 允許的最大登入失敗次數
+	NotFound404            int           `json:"not_found_404"`             // 單一 Session 允許的最大 404 請求數
+	ScoreNormal            int           `json:"score_normal"`              // 正常請求的風險分數
+	ScoreSuspicious        int           `json:"score_suspicious"`          // 可疑請求的風險分數
+	ScoreDangerous         int           `json:"score_dangerous"`           // 危險請求的風險分數
+	ScoreSessionMultiIP    int           `json:"score_session_multi_ip"`    // 單一 Session 允許的最大 IP 數可疑分數
+	ScoreIPMultiDevice     int           `json:"score_ip_multi_device"`     // 單一 IP 允許的最大設備數可疑分數
+	ScoreDeviceMultiIP     int           `json:"score_device_multi_ip"`     // 單一設備允許的最大 IP 數可疑分數
+	ScoreFpMultiSession    int           `json:"score_fp_multi_session"`    // 單一指紋允許的最大 Session 數可疑分數
+	ScoreGeoHighRisk       int           `json:"score_geo_high_risk"`       // 高風險地理位置可疑分數
+	ScoreGeoHopping        int           `json:"score_geo_hopping"`         // 地理位置跳躍可疑分數
+	ScoreGeoFrequentSwitch int           `json:"score_geo_frequent_switch"` // 地理位置頻繁切換可疑分數
+	ScoreGeoRapidChange    int           `json:"score_geo_rapid_change"`    // 地理位置快速變化可疑分數
+	ScoreIntervalRequest   int           `json:"score_interval_request"`    // 短時間內的請求數可疑分數
+	ScoreFrequencyRequest  int           `json:"score_frequency_request"`   // 請求頻率可疑分數
+	ScoreLongConnection    int           `json:"score_long_connection"`     // 長連接可疑分數
+	ScoreLoginFailure      int           `json:"score_login_failure"`       // 登入失敗可疑分數
+	ScoreNotFound404       int           `json:"score_not_found_404"`       // 404 請求可疑分數
 }
 
 type IPGuardian struct {
-	Context context.Context
-	Config  *Config
-	Redis   *redis.Client
-	Logger  *Logger
-	// GeoChecker   *GeoCheck
+	Context  context.Context
+	Config   *Config
+	Redis    *redis.Client
+	Logger   *Logger
+	GeoLite2 *GeoLite2
+	Manager  *Manager
 	// AbuseIPDBApi *AbuseIPDBApi
-	Manager *Manager
 }
 
 type Manager struct {
-	Trust *TrustManager
-	Ban   *BanManager
-	Block *BlockManager
+	Allow *AllowIPManager
+	Block *BlockIPManager
+	Deny  *DenyIPManager
 }
-
-// type GeoCheck struct {
-// 	DB                *geoip2.Reader
-// 	highRiskCountries map[string]bool
-// 	proxyRanges       []string // 代理 IP 範圍
-// 	vpnRanges         []string // VPN IP 範圍
-// }
-
-// type GeoRisk struct {
-// 	HighRiskCountries []string `json:"high_risk_countries"`
-// 	ProxyDetection    bool     `json:"proxy_detection"`
-// 	VPNDetection      bool     `json:"vpn_detection"`
-// }
 
 type Redis struct {
 	Host     string `json:"host"`
 	Port     int    `json:"port"`
 	Password string `json:"password"`
 	DB       int    `json:"db"`
+}
+
+type IPItem struct {
+	IP      string `json:"ip"`
+	Reason  string `json:"reason"`
+	AddedAt int64  `json:"added_at"`
+	Count   int    `json:"count,omitempty"`
+	Last    int64  `json:"last,omitempty"`
 }
 
 // type AbuseIPDBApi struct {